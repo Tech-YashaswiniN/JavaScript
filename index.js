a74a074c88725182ad278c61192a168b4c5d767e
Tech-YashaswiniN <yashawininanjappajigithub@gmail.com>
2023-11-19 01:32:49 +0530

    Js file

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-let FirstName = prompt("Enter your FirstName");
-alert(`Welcome ${FirstName}`)
+let Name = prompt("Enter your FirstName");
+let display = `Welcome ${Yasahswini}`;
+alert(display);
+let a = 10;
+let b = 20;
+let result = 10 % 20;
+console.log(result);
