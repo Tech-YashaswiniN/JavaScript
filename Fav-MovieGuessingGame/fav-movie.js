81e32e77abec8d202f296ff84d1e842c3b547073
Tech-YashaswiniN <yashawininanjappajigithub@gmail.com>
2023-12-02 13:02:37 +0530

    Guessing-Game

---
+++ Fav-MovieGuessingGame/fav-movie.js
@@ -0,0 +1,12 @@
+const favMovie = "3 idiots";
+
+let guess = prompt("Guess My favorite movie");
+
+while ((guess != favMovie) && (guess != "quit")) {
+    guess = prompt("Wrong guess, Please try again! if you don't want to play you can quit by saying 'quit' ")
+}
+if (guess === favMovie) {
+    console.log("Congrats!");
+} else {
+    console.log("Ypu quit the game");
+}
