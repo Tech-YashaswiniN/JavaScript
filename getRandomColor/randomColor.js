8904d56299f5be048583044ab79d80947295a0b8
Tech-YashaswiniN <yashawininanjappajigithub@gmail.com>
2024-10-02 00:54:04 +0530

    RandomColor

--- getRandomColor/randomColor.js
+++ getRandomColor/randomColor.js
@@ -1,19 +1,42 @@
-let btn = document.querySelector("button");
+let colorList = [];
 
-btn.addEventListener("click", function () {
-    let h1 = document.querySelector("h1");
-    let randomColor = getRandomColor();
-    h1.innerText = randomColor;
+let btn = document.querySelector(".genColor");
 
-    let div = document.querySelector("div");
-    div.style.backgroundColor = randomColor;
-});
+btn.addEventListener("click",function(){
+    let h3 = document.querySelector('h3');
+    let randomColor =getRandomColor();
+    h3.innerText=randomColor;
 
-function getRandomColor() {
-    let red = Math.floor(Math.random() * 255);
-    let green = Math.floor(Math.random() * 255);
-    let blue = Math.floor(Math.random() * 255);
+    let colorBox = document.querySelector('.container div');
+    colorBox.style.backgroundColor= randomColor;
+
+    let colors = randomColor;
+    colorList.push(colors);
+
+})
+
+
+function getRandomColor(){
+    let red = Math.floor(Math.random()*255);
+    let green = Math.floor(Math.random()*255);
+    let blue = Math.floor(Math.random()*255);
 
     let color = `rgb(${red},${green},${blue})`;
-    return color;
+    return color
 }
+
+let prevBtn = document.querySelector('.prevColor');
+
+prevBtn.addEventListener('click',function(){
+    let colorBox = document.querySelector('.container div');
+    console.log(colorList.slice(colorList.length-2, colorList.length-1))
+    let prevclr = colorList.slice(colorList.length-2, colorList.length-1);
+    colorBox.style.backgroundColor = prevclr;
+
+    let h3 = document.querySelector('h3');
+    h3.innerText = prevclr;
+})
+
+
+
+
